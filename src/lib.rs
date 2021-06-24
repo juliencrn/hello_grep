5f52faa8b531e46aea7aa9c86aae95878070c3ac
Julien <juliencaron@protonmail.com>
2021-06-25 01:27:51 +0200

    Add test in lib.rs and create search fn

--- src/lib.rs
+++ src/lib.rs
@@ -21,7 +21,69 @@ impl Config {
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let content = fs::read_to_string(config.path)?;
 
-    println!("File content:\n{}", content);
+    for line in search(&config.query, &content) {
+        println!("{}", line)
+    }
 
     Ok(())
+}
+
+pub fn search<'a>(query: &str, content: &'a str) -> Vec<&'a str> {
+    let mut results: Vec<&'a str> = vec![];
+
+    for line in content.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+    
+    results
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn create_test_config() -> Config {
+        let args: Vec<String> = vec![
+            String::from("path/to/bin"),
+            String::from("run"),
+            String::from("./src/lib.rs"),
+        ];
+
+        Config::new(&args).unwrap()
+    }
+
+    #[test]
+    fn create_config() {
+        let config = create_test_config();
+
+        assert_eq!(config.query, "run");
+        assert_eq!(config.path, "./src/lib.rs");
+    }
+
+    #[test]
+    #[should_panic]
+    fn create_config_without_args() {
+        let args: Vec<String> = vec![];
+        Config::new(&args).unwrap();
+    }
+
+    #[test]
+    fn run_should_not_panic() -> Result<(), String> {
+        let config = create_test_config();
+        run(config).unwrap();
+        Ok(())
+    }
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
 }
